508b56e827e8cdfa9069eda427e9d33c80de6487
Niklas Verenkotte <niklas.verenkotte@check24.de>
2025-10-07 15:21:27 +0200

    Show a bit more width

--- app/routes/show/components/QuestionTypes/BaseQuestionShow.tsx
+++ app/routes/show/components/QuestionTypes/BaseQuestionShow.tsx
@@ -94,7 +94,7 @@ const BaseQuestionShow = ({
   return (
     <div
       className={
-        "bg-gray-800 border-teal-700 border-4 rounded-3xl w-7/8 flex-1 min-h-0 self-center flex flex-col text-5xl"
+        "bg-gray-800 border-teal-700 border-4 rounded-3xl w-[95%] flex-1 min-h-0 self-center flex flex-col text-5xl"
       }
     >
       {withHeader && (
